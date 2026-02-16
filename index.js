428b7fdff34dc1c8750ffd84560f95d930d5d564
Kendra Allen <kallen5@ufl.edu>
2026-02-15 22:40:05 -0500

    modified index.html

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ document.getElementById("searchBtn").addEventListener("click", function() {
 
     window.location.href = `explore.html?city=${encodeURIComponent(city)}`;
 });
+
+if (window.location.pathname.includes(explore.html)) {
+    const param = new URLSearchParams(window.location.search);
+    const city = URLSearchParams.get("city");
+
+    const resultsDiv = document.getElementById("results");
+
+    if (city) {
+        resultsDiv.innerHTML = `<h2>Showing neighborhoods in ${city}</h2>`;
+    } else {
+        resultsDiv.innerHTML = "<p>Please search for a city first.</p>";
+    }
+}
